523345adac7d341a58598e1c1d02b4b821cb43a1
Pedro Barreto <pedrosousabarreto@gmail.com>
2019-02-06 11:29:13 +0000

    another try at the types generation #2

--- src/index.ts
+++ src/index.ts
@@ -17,19 +17,30 @@ import * as imp_ServiceParams from "./service_params";
 	export type IDiFactory = Interfaces.IDiFactory;
 	export type IConfigsProvider = Interfaces.IConfigsProvider;
 
-	export type ConsoleLogger = imp_ConsoleLogger
+	export type ConsoleLogger = imp_ConsoleLogger;
+	export const ConsoleLogger = imp_ConsoleLogger;
+
 	export type DiContainer = imp_DiContainer;
+	export const DiContainer = imp_DiContainer;
+
 	export type Microservice = imp_Microservice;
+	export const Microservice = imp_Microservice;
 
 	export type ServiceConfigs = imp_ServiceConfigs.ServiceConfigs;
+	export const ServiceConfigs = imp_ServiceConfigs.ServiceConfigs;
 	export type AppBaseConfigs = imp_ServiceConfigs.AppBaseConfigs;
+	export const AppBaseConfigs = imp_ServiceConfigs.AppBaseConfigs;
 
 
 	export type ServiceParams = imp_ServiceParams.ServiceParams;
+	export const ServiceParams = imp_ServiceParams.ServiceParams;
 	export type ServiceParam = imp_ServiceParams.ServiceParam;
+	export const ServiceParam = imp_ServiceParams.ServiceParam;
 	export type ServiceFeatureFlag = imp_ServiceParams.ServiceFeatureFlag;
+	export const ServiceFeatureFlag = imp_ServiceParams.ServiceFeatureFlag;
 
 	export type ParamType = imp_ServiceParams.ParamType;
+
 	export const PARAM_TYPES = imp_ServiceParams.PARAM_TYPES;
 
 // }
