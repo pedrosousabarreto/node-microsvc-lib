eb991a75aad88c1bc765c3b32f2856426027eda9
Dima <kondrashov.do@gmail.com>
2019-03-27 15:24:01 +0100

    add types, fix secret_loader callbacks

--- src/config_providers/aws_secret_loader.ts
+++ src/config_providers/aws_secret_loader.ts
@@ -8,7 +8,7 @@ export class AWSSecretProvider implements IConfigsProvider {
   private _role_arn: string;
   private _role_session_name: string;
   private _region: string;
-  private _kvs: any;
+  private _kvs: Map<string, string>;
   private _secret_name: string;
 
   public get solution_name(): string {
@@ -25,7 +25,7 @@ export class AWSSecretProvider implements IConfigsProvider {
     this._role_arn = role_arn;
     this._role_session_name = role_session_name;
     this._region = region;
-    this._kvs = {};
+    this._kvs = new Map<string, string>();
     this._secret_name = solution_name;
   }
 
@@ -34,7 +34,7 @@ export class AWSSecretProvider implements IConfigsProvider {
   }
 
   get_value(key_name: string): string | null {
-    return this._kvs[key_name] || null;
+    return this._kvs.get(key_name) || null;
   }
 
   private _fetch_all_from_aws_secrets_manager(
@@ -42,49 +42,38 @@ export class AWSSecretProvider implements IConfigsProvider {
     callback: (err?: Error) => void
   ) {
     let sts = new AWS.STS({});
-
     let role_params = {
       RoleArn: this._role_arn,
       RoleSessionName: this._role_session_name
     };
     sts.assumeRole(role_params, (err, data) => {
       if (err) {
-        // an error occurred
-        console.log(err, err.stack);
         return callback(err);
+      } else {
+        // successful response
+        let secrets_manager = new AWS.SecretsManager({
+          region: this._region,
+          ...data.Credentials
+        });
+        let secrets: any = {};
+        secrets_manager.getSecretValue(
+          { SecretId: this._secret_name },
+          (err, secret_data) => {
+            if (err) {
+              return callback(err);
+            } else {
+              // successful response
+              secrets = JSON.parse(String(secret_data.SecretString));
+            }
+            keys.forEach((key: string) => {
+              if (key in secrets) {
+                this._kvs.set(key, secrets[key]);
+              }
+            });
+            callback();
+          }
+        );
       }
-      // successful response
-      else console.log(data);
-      let secrets_manager = new AWS.SecretsManager({
-        region: this._region,
-        ...data.Credentials
-      });
-      const secrets = this._get_secrets(secrets_manager, this._secret_name);
-      keys.forEach((key: string) => {
-        if (key in secrets) {
-          this._kvs[key] = secrets[key];
-        }
-      });
-      callback();
     });
-  }
-
-  private _get_secrets(
-    secrets_manager: AWS.SecretsManager,
-    secret_id: string
-  ): any {
-    var secrets = {};
-    secrets_manager.getSecretValue({ SecretId: secret_id }, function(
-      err,
-      secret_data
-    ) {
-      if (err)
-        // an error occurred
-        console.log(err, err.stack);
-      // successful response
-      else console.log(secret_data);
-      secrets = JSON.parse(String(secret_data.SecretString));
-    });
-    return secrets;
   }
 }
