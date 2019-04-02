2452eafa7fa4310efe4d259f6e977dcfd6d2557d
Pedro Barreto <pedrosousabarreto@gmail.com>
2019-04-02 15:13:49 +0100

    old comment cleanup

--- src/tests/config/config.ts
+++ src/tests/config/config.ts
@@ -16,12 +16,6 @@ app_base_confs.app_version = "0.0.1";
 app_base_confs.app_api_prefix = "";
 app_base_confs.app_api_version = "1";
 
-//
-// // First load the required params with their default values
-// import svc_params = require("./params");
-// // check if overrides is enabled and an override file exists and if so, apply it
-// svc_params.override_from_env_file(app_base_confs);
-
 
 /*
 * inject these env vars:
